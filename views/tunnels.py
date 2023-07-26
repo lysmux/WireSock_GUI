1b71c7e714743ef2039664720c1e9303e4af4bcc
KllRaz <kllraz@yandex.ru>
2023-07-26 10:59:23 +0300

    * Added "Edit tunnel" button and "Add tunnel" filepicker

--- views/tunnels.py
+++ views/tunnels.py
@@ -11,10 +11,11 @@ class TunnelsView(flet.UserControl):
         super().__init__()
         self.expand = True
 
+        self.info_column = flet.Column()
+        self.file_picker = flet.FilePicker(on_result=self.on_tunnel_add)
+
         self.tunnels = [wiresock_manager.load_config(pathlib.Path("conf")),
                         wiresock_manager.load_config(pathlib.Path("pc.conf"))]
-
-        self.info_column = flet.Column()
 
     def build(self):
         tunnels_view = flet.Column(scroll=flet.ScrollMode.AUTO, expand=True)
@@ -27,7 +28,9 @@ class TunnelsView(flet.UserControl):
         return flet.Row([
             flet.Column([
                 tunnels_view,
-                flet.ElevatedButton(text="Add tunnel")
+                self.file_picker,
+                flet.ElevatedButton(text="Add tunnel",
+                                    on_click=lambda _: self.file_picker.pick_files(allowed_extensions=["conf"]))
             ]),
             flet.VerticalDivider(width=9, thickness=3),
             self.info_column
@@ -37,23 +40,23 @@ class TunnelsView(flet.UserControl):
         tunnel = event.control.data
 
         self.info_column.controls = [
-            flet.Text(value="Interface",
+            flet.Text(value="Interface:",
                       weight=flet.FontWeight.BOLD,
                       size=40),
             flet.Row([
-                flet.Text(value="PrivateKey"),
+                flet.Text(value="PrivateKey:"),
                 flet.Text(value=tunnel.interface.private_key),
             ]),
             flet.Row([
-                flet.Text(value="Address"),
+                flet.Text(value="Address:"),
                 flet.Text(value=tunnel.interface.address),
             ]),
             flet.Row([
-                flet.Text(value="DNS"),
+                flet.Text(value="DNS:"),
                 flet.Text(value=tunnel.interface.dns),
             ]),
             flet.Row([
-                flet.Text(value="MTU"),
+                flet.Text(value="MTU:"),
                 flet.Text(value=tunnel.interface.mtu),
             ]),
 
@@ -61,25 +64,33 @@ class TunnelsView(flet.UserControl):
                       weight=flet.FontWeight.BOLD,
                       size=40),
             flet.Row([
-                flet.Text(value="PublicKey"),
+                flet.Text(value="PublicKey:"),
                 flet.Text(value=tunnel.peer.public_key),
             ]),
             flet.Row([
-                flet.Text(value="Endpoint"),
+                flet.Text(value="Endpoint:"),
                 flet.Text(value=tunnel.peer.endpoint),
             ]),
             flet.Row([
-                flet.Text(value="AllowedIPs"),
+                flet.Text(value="AllowedIPs:"),
                 flet.Text(value=tunnel.peer.allowed_ips),
             ]),
             flet.Row([
-                flet.Text(value="DisallowedIPs"),
+                flet.Text(value="DisallowedIPs:"),
                 flet.Text(value=tunnel.peer.disallowed_ips),
             ]),
             flet.Row([
-                flet.Text(value="AllowedApps"),
+                flet.Text(value="AllowedApps:"),
                 flet.Text(value=tunnel.peer.allowed_apps),
             ]),
+
+            flet.ElevatedButton(text="Edit tunnel", on_click=self.on_tunnel_edit, data=tunnel)
         ]
 
         self.info_column.update()
+
+    def on_tunnel_add(self, event: flet.FilePickerResultEvent):
+        pass
+
+    def on_tunnel_edit(self, event: flet.ControlEvent):
+        pass
