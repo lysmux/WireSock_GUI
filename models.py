b92b05d9c2d142b881e00ee44fc6a4b30a2b776e
KllRaz <kllraz@yandex.ru>
2023-08-01 17:58:10 +0300

    change default values for models

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass, field
 @dataclass
 class Interface:
     private_key: str = field(default="")
-    address: list[str] = field(default_factory=list)
-    dns: list[str] = field(default_factory=list)
+    address: list[str] = field(default_factory=lambda: [])
+    dns: list[str] = field(default_factory=lambda: [])
     mtu: int = field(default=1280)
 
 
@@ -14,10 +14,10 @@ class Peer:
     public_key: str = field(default="")
     pre_shared_key: str = field(default="")
     endpoint: str = field(default="")
-    allowed_ips: list[str] = field(default_factory=list)
-    disallowed_ips: list[str] = field(default_factory=list)
-    allowed_apps: list[str] = field(default_factory=list)
-    disallowed_apps: list[str] = field(default_factory=list)
+    allowed_ips: list[str] = field(default_factory=lambda: [])
+    disallowed_ips: list[str] = field(default_factory=lambda: [])
+    allowed_apps: list[str] = field(default_factory=lambda: [])
+    disallowed_apps: list[str] = field(default_factory=lambda: [])
     persistent_keepalive: int = field(default=25)
 
 
